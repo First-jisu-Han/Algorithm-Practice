6f8a32774257c1dd98e9d16207ad22d199948c45
Jisu Han <weblite@naver.com>
2021-11-03 15:57:53 +0900

    dp 피보나치 수열에서의 재귀비효율성

---
+++ src/dp_practice/fibonacci.java
@@ -0,0 +1,28 @@
+package dp_practice;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        System.out.println(recurfibo(7));
+        System.out.println(iterfibo(7));
+    }
+
+    public static int recurfibo(int n){
+        if(n==1 || n==2) return 1;
+        return recurfibo(n-1)+recurfibo(n-2);
+    }
+
+    public static int iterfibo(int n){
+        int[] numArr=new int[n+1];
+        numArr[0]=0;
+        numArr[1]=1;
+        numArr[2]=1;
+        for(int i=3;i<=n;i++){
+            numArr[i]=numArr[i-1]+numArr[i-2];
+        }
+        return numArr[n];
+
+
+    }
+
+
+}
