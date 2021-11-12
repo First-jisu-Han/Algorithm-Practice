d7301b8f9c4153f7ce174836a9dda0dc5e8fb920
Jisu Han <weblite@naver.com>
2021-11-13 02:00:14 +0900

    정수를 나눠서 곱하는 법 - 몫고 나머지이용

---
+++ src/beakjoon/BJ2588.java
@@ -0,0 +1,20 @@
+package beakjoon;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BJ2588 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int k=num2;
+        while(k>0){
+            System.out.println(num1*(k%10));
+            k=k/10;
+        }
+
+        System.out.print(num1*num2);
+    }
+}
