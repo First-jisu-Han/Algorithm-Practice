215fc372e775fed0f57d9c7025240bb6a5eeea06
Jisu Han <weblite@naver.com>
2021-11-17 23:09:45 +0900

    1009 풀다말앗

--- src/beakjoon/BJ1009.java
+++ src/beakjoon/BJ1009.java
@@ -4,27 +4,26 @@ import java.util.Scanner;
 
 public class BJ1009 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int buffer = sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int buffer=sc.nextInt();
         sc.nextLine();             // 직동 오류 방지
-        int[] arr = new int[buffer * 2];
-        String[] arrStr = new String[buffer * 2];
-        for (int i = 0; i < buffer; i++) {
-            String str = sc.nextLine();
-            arrStr = str.split(" ");
-            arr[i] = Integer.parseInt(arrStr[0]);
-            arr[i + 1] = Integer.parseInt(arrStr[1]);
-        }
-        double[] ans=new double[buffer];
-        for(int j=0;j<buffer*2;j+=2) {
-            double num=Math.pow(arr[j], arr[j + 1]);
-            while(num>=10){
-                num/=10;
+        double [] ans=new double[buffer];
+        for(int i=0;i<buffer;i++){
+            String str=sc.nextLine();
+            int arr[]=new int [2];
+            String[] arrSt;
+            arrSt=str.split(" ");
+            for(int j=0;j<2 ;j++){
+                arr[j]=Integer.parseInt(arrSt[j]);
             }
-            ans[j]=num%10;
+            int k=(int)Math.pow(arr[0],arr[1]);
+            ans[i]=k%10;
         }
         for(double an:ans){
             System.out.println(an);
         }
+
+
+
 
     }}
