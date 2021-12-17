c5cf08bd5b401eced1d60c2f9ef8cf4418449d0e
Jisu Han <weblite@naver.com>
2021-12-17 15:11:46 +0900

    간단한 java 기술면접구현 30초안쪽 코드 구현

--- src/java_grammer_basic/Practice.java
+++ src/java_grammer_basic/Practice.java
@@ -1,9 +1,35 @@
 package java_grammer_basic;
+import java.util.Scanner;
+
+// java 자료구조 + 알고리즘 기술면접구현 + 알고리즘  chapter 1/11
 public class Practice {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         System.out.println(deMorgan(false ,false));
-        System.out.print(deMorgan( true,false));
-
+        System.out.println(deMorgan( true,false));
+        int width=sc.nextInt();
+        for(int i=0;i<width;i++){
+            for(int j=0;j<width;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        int triangleWidth=sc.nextInt();
+        for(int k=0; k<triangleWidth;k++){
+            for(int l=0;l<=k;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // 역순별
+        for(int i=triangleWidth-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        npira(5);
     }
 //    De Morgan's laws  - 드 모르강의 법칙 ,간단한 집합 법칙 !와 조건 혼합시 알아둬야할 부분
     static boolean deMorgan(boolean a, boolean b){
@@ -15,5 +41,17 @@ public class Practice {
         }
         return a;
     }
+    static void npira(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+
+    }
+
+
+
 
 }
