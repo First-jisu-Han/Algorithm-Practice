2d5fb7faa8f79fd5e8b95050f2b83978ef0d9881
Jisu Han <weblite@naver.com>
2021-12-25 02:02:53 +0900

    입력값 이내의 소수 모두 출력하는 메서드 구현 -4분 이내에 구현 (3분 소요 )

--- src/java_grammer_basic/PracticeChap3_1.java
+++ src/java_grammer_basic/PracticeChap3_1.java
@@ -1,11 +1,29 @@
 package java_grammer_basic;
 
+import java.util.Scanner;
+
+// 1000까지 소수 구하기 코드
 public class PracticeChap3_1 {
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        getPrimeNumber(number);
     }
-    static void getMethod(){
-        
-    }
+    // 약수가 총 두개밖에 없다는 발상 , 3분 소요
+    static void getPrimeNumber(int range){
+        for(int i=1; i<=range;i++) {
+            int cnt=0;
+            for (int j = 1; i >= j; j++) {
+                if (i % j == 0) {
+                    cnt+= 1;
+                }
+            }
+            if (cnt==2) {
+                System.out.printf("%d 사이의 소수인 %d %n", range, i);
+            }
+        }
+            }
 
-}
+        }
+
+
