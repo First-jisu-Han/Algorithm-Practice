8e3ac699abcd2e803a066659844e3868a112001b
Jisu Han <weblite@naver.com>
2021-12-25 02:23:54 +0900

    소수 구하는 문제 숏코딩 예시답안 - 내가 푼 방식과 다름 ( 약수 2개 x vs 자신과 1 제외해서 약수 없도록 )

--- src/java_grammer_basic/PracticeChap3_1.java
+++ src/java_grammer_basic/PracticeChap3_1.java
@@ -7,7 +7,8 @@ public class PracticeChap3_1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int number=sc.nextInt();
-        getPrimeNumber(number);
+//        getPrimeNumber(number);
+        getPrimeNumberAnother(number);
     }
     // 약수가 총 두개밖에 없다는 발상 , 3분 소요
     static void getPrimeNumber(int range){
@@ -15,15 +16,31 @@ public class PracticeChap3_1 {
             int cnt=0;
             for (int j = 1; i >= j; j++) {
                 if (i % j == 0) {
-                    cnt+= 1;
+                    cnt += 1;
                 }
             }
-            if (cnt==2) {
+            if (cnt == 2) {
                 System.out.printf("%d 사이의 소수인 %d %n", range, i);
             }
         }
-            }
+    }
 
+    // 1과 자신을 제외한 수 중에 약수가 존재하지 않으면 소수라는 발상
+    static void getPrimeNumberAnother(int range){
+        for (int i = 2; i <= range; i++) {
+            int j;
+            for (j = 2; j<i ; j++) {
+                if (i % j == 0) {
+                    break;
+                }
+            }
+            // 끝까지 돌았으면 출력 ( 끝까지 돌기 전에 break 된 것- 약수가 있는 수들은 j와 i가 달라서 출력 못함 )
+            if(j==i) System.out.printf("%d 사이의 소수인 %d %n", range, i);
+            }
         }
+    }
+
+
+
 
 
