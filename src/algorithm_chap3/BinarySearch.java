dbffb8c9d4f3b36cc20bbd2338b8ded49117b636
Jisu Han <weblite@naver.com>
2022-01-14 00:06:21 +0900

    알고리즘/ 자료구조 chap3 완료

--- src/algorithm_chap3/BinarySearch.java
+++ src/algorithm_chap3/BinarySearch.java
@@ -1,10 +1,16 @@
 package algorithm_chap3;
 
+import java.util.Arrays;
+
 // 이진 검색 빠르게 구현
 public class BinarySearch {
     public static void main(String args[]){
         int [] arr = {200,300,400,500,555,3354,33355,56666666};
         System.out.println(search(arr,3354));
+        System.out.println(Arrays.binarySearch(arr,3354)+1);    // java의 표준 라이브러리가 제공하는 메서드 - 자동 이진검색
+        System.out.println(Arrays.binarySearch(arr,666)); // 배열이 없는 경우 삽입될 위치를 음수로 return 한다. -index-1 을 리턴 프린트결과 -6
+
+
     }
 
     public static int search(int [] a ,int target){
---
+++ src/algorithm_chap3/Id.java
@@ -0,0 +1,52 @@
+package algorithm_chap3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Id {
+    private static int counter=0; // 클래스 변수
+    private String[] information={"abc","bcd","string","research","zookeeper"};   // 인스턴스 변수
+    private int age;
+    public Id(int age){
+        counter++; // 생성자
+        this.age=age;
+    }
+    // 클래스 메서드
+    public static int getCounter(){
+        return counter;
+    }
+    // 인스턴스 메서드
+    public String[] getInformation(){
+        return information;
+    }
+
+    public static final Comparator<Id> COMPARATOR=new Comp();
+
+    static class idTest {
+        public static void main(String[] args) {
+            Id[] id = {
+                    new Id(12),
+                    new Id(23),
+                    new Id(33),
+                    new Id(2),
+                    new Id(1)
+            };
+            System.out.println(Arrays.binarySearch(id[0].getInformation(), "bcd")); // String 에서도 key값으로 인덱스 위치 찾을 수 있다.
+            System.out.println(Id.getCounter());
+            System.out.println(Arrays.binarySearch(id,new Id(1), COMPARATOR)); // 내림차순 정렬했기때문에
+        }
+    }
+    private static class Comp implements Comparator<Id>{
+        @Override
+        public int compare(Id d1, Id d2) {
+            if(d1.age<d2.age){
+                return 1;
+            } else if(d1.age>d2.age) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+}
