e4b7245693761334ba4308220b17d2b55bdd4d1b
Jisu Han <weblite@naver.com>
2022-02-22 00:34:59 +0900

    자바의 정석 스터디 Exception Handling

--- src/자바의정석/chapter8_exception/ExceptionTest.java
+++ src/자바의정석/chapter8_exception/ExceptionTest.java
@@ -13,7 +13,33 @@ public class ExceptionTest {
                 System.out.println(e.getStackTrace());
                 System.out.println(e.getMessage());
             }
-
         }
+    }
+}
+// 예외 발생시키기
+class exceptionMaking {
+    public static void main(String[] args) {
+            try{
+                throw new Exception("고의 발생시킴");
+            } catch(Exception e){
+                System.out.print("에러메세지:"+e.getMessage());
+                e.printStackTrace();
+            }
+            System.out.println("프로그램 정상 종료");
+    }
+}
+
+// 예외를 메서드에 선언
+class MethodException {
+    public static void main(String[] args) {
+        try{
+            method1();
+        } catch(Exception e){
+            System.out.println("method 1에서 발생한 오류를 잡음 오류 메세지 : "+ e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    static void method1() throws Exception {
+        throw new Exception("예외 던진다.");
     }
 }
