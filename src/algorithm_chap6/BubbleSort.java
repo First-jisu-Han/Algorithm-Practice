0c425514f1bccb60010f85952f802b03e005dd0b
Jisu Han <weblite@naver.com>
2022-02-25 18:58:19 +0900

    알고리즘 연습 - 버블정렬 알고리즘 시각화

--- src/algorithm_chap6/BubbleSort.java
+++ src/algorithm_chap6/BubbleSort.java
@@ -4,10 +4,10 @@ package algorithm_chap6;
 public class BubbleSort {
     public static void main(String[] args) {
         int intArr[] = {100, 90, 200, 500, 110, 20, 3, 5, 7, 1};
-        bubbleSort(intArr);
-        for (int arr : intArr) {
-            System.out.print(arr + " ");
-        }
+//        bubbleSort(intArr);
+//        for (int arr : intArr) {
+//            System.out.print(arr + " ");
+//        }
         bubbleSortingVisualization(intArr);
     }
 
@@ -30,10 +30,21 @@ public class BubbleSort {
     public static void bubbleSortingVisualization(int[] intArr) {
         for (int i = intArr.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
+                for (int k = 0; k < j; k++) {
+                    System.out.print(intArr[k] + " ");
+                }
                 if (intArr[j] > intArr[j + 1]) {
                     swap(intArr, j, j + 1);
+                    System.out.print(intArr[j]+"+");
                 }
-                System.out.print(intArr[j]);
+                else{
+                    System.out.print(intArr[j]+"-");
+                }
+                for(int m=j+1;m<intArr.length;m++){
+                    System.out.print(intArr[m]+" ");
+                }
+                System.out.println();
+
             }
         }
     }
